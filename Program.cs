41a4081f31f771a42e9b56899c5ebddf89226d43
Charles Pao <windperson@gmail.com>
2016-07-17 13:15:38 +0800

    init repo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var welcome = "Hello World!";
+            Console.WriteLine(welcome);
+        }
+    }
+}
